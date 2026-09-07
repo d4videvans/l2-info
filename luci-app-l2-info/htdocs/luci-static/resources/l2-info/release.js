dbf347745bc8f087cfc8130efe833c4b671d2322
d4videvans <d@videvans.com>
2026-09-07 22:38:51 +0100

    Match LuCI module style for release identity

--- luci-app-l2-info/htdocs/luci-static/resources/l2-info/release.js
+++ luci-app-l2-info/htdocs/luci-static/resources/l2-info/release.js
@@ -3,8 +3,9 @@
  * LuCI Makefile; tests/run.sh enforces that relationship and the public docs. */
 
 'use strict';
+'require baseclass';
 
-return {
+return baseclass.extend({
 	tag: 'v0.1.0-rc3',
 	packageVersion: '0.1.0-r3'
-};
+});
